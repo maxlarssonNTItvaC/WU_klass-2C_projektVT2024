7dc4edd19e2c53a2b3c5a13fb006dfb290a10e61
maxlarssonNTItvaC <150041214+maxlarssonNTItvaC@users.noreply.github.com>
2024-03-29 20:26:06 +0100

    2024 03 29

--- js/script.js
+++ js/script.js
@@ -1,30 +1,8 @@
-function addToCart(product) {
-    // Hämta produktinformation (t.ex. namn, pris, ID)
-    const name = product.find(".product-name").text();
-    const price = product.find(".product-price").text();
-    const id = product.data("id");
-  
-    // Skapa ett objekt för varukorgen
-    const cartItem = {
-      name,
-      price,
-      id,
-      quantity: 1,
-    };
-  
-    // Skicka objektet till varukorgssidan (t.ex. via AJAX)
-    $.ajax({
-      url: "/varukorg/add",
-      method: "POST",
-      data: cartItem,
-      success: function() {
-        // Visa en bekräftelse till användaren
-        alert("Produkten har lagts till i varukorgen!");
-      },
-    });
-  }
+const burger = document.getElementById('burger')
+const linkList = document.getElementById('link_list')
 
-  $(".product").on("click", function() {
-    addToCart($(this));
-  });
-  
+function show() {
+
+  linkList.classList.toggle('links_visible')
+  burger.classList.toggle('burger_in_x_form')
+}
